c00122bd4dd87d04c83ff0683664f1bee48d9a75
MysteryMan2653 <36777621+MysteryMan2653@users.noreply.github.com>
2018-02-23 14:56:19 -0800

    Add files via upload

--- Quicksort.java
+++ Quicksort.java
@@ -1,5 +1,21 @@
 public class Quicksort {
-    public static void main (String [] args) {
-    
+    private static void quickSort(int[] a, int left, int right) {
+        if (left >= right) return;
+        int i = left;
+        int j = right;
+        int pivotValue = a[(left + right) / 2];
+        while (i < j) {
+            while (a[i] < pivotValue) i++;
+            while (pivotValue < a[j]) j--;
+            if (i <= j) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        quickSort(a, left, j);
+        quickSort(a, i, right);
     }
 }
